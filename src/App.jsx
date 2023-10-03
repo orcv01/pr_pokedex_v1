219ef0ce03d3ddff120e8ca2c34160f98465b575
valeeria15 <valeria.colmenares.figueroa@alumnos.upm.es>
2023-10-03 23:04:26 +0200

    Upload pr_pokedex_v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,54 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const App = () => {
-  const [pokemonName, setPokemonName] = useState("");
-  const [pokemonChosen, setPokemonChosen] = useState(false);
-  const [pokemon, setPokemon] = useState({
-                                          name: "",
-                                          number: "",
-                                          species: "",
-                                          image: "",
-                                          hp: "",
-                                          attack: "",
-                                          defense: "",
-                                          speed: "",
-                                          type: "",
-                                        });
-
-  const searchPokemon = () => {axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-                          .then((res) => {setPokemon({
-                                          name: pokemonName,
-                                          number: res.data.id,
-                                          species: res.data.species.name,
-                                          image: res.data.sprites.front_default,
-                                          hp: res.data.stats[0].base_stat,
-                                          attack: res.data.stats[1].base_stat,
-                                          defense: res.data.stats[2].base_stat,
-                                          speed: res.data.stats[5].base_stat,
-                                          type: res.data.types[0].type.name,
-                                        });
-                                        setPokemonChosen(true);
-                                })};
+const PokemonSearch = () => {
+  const [name, setName] = useState("");
+  const [pokemons, setPokemons] = useState([]);
   
+  const pokeCarga = async () => {
+    const response = await 
+    axios.get(`https://pokeapi.co/api/v2/pokemon/?name=${name}`);
+    setPokemons(response.data.results);
+  };
 
-
-return (
-  
+  return (
   <div className="App">
     <div className="TitleSection">
       <h1>Pokédex</h1>
-      <input type="text" onChange={(event) => {setPokemonName(event.target.value)}}
-      value={pokemonName.toLowerCase()}/>
       <div>
-        {pokemonName && <button onClick={searchPokemon}>Search Pokémon</button>}
-      </div>
-    
-      <div className="DisplaySection">
-        {!pokemonChosen ? (<h2> Please choose a Pokémon</h2>) 
-                        : (
-                          <>
-                            <h2>{pokemon.name}</h2>
-                            <img src={pokemon.image} alt={pokemon.name} />
-                            <h3>Number: #{pokemon.number}</h3>
-                            <h3>Species: {pokemon.species}</h3>
-                            <h3>Type: {pokemon.type}</h3>
-                            <h4>Hp: {pokemon.hp}</h4>
-                            <h4>Attack: {pokemon.attack}</h4>
-                            <h4>Defense: {pokemon.defense}</h4>
-                            <h4>Speed: {pokemon.speed}</h4>
-                          </>
-                        )}
+        <input
+          type="text"
+          placeholder="Nombre del Pokémon"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div>
+          {name && <button onClick={pokeCarga}>Buscar</button>}
+        </div>
+        <div className="DisplaySection">
+        
+        <ul>
+          {pokemons.map((pokemon) => (
+            <li key={pokemon.id}>
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.image} alt={pokemon.name} />
+              <h3>Number: #{pokemon.number}</h3>
+              <h3>Species: {pokemon.species}</h3>
+              <h3>Type: {pokemon.type}</h3>
+              <h4>Hp: {pokemon.hp}</h4>
+              <h4>Attack: {pokemon.attack}</h4>
+              <h4>Defense: {pokemon.defense}</h4>
+              <h4>Speed: {pokemon.speed}</h4>
+            </li>
+          ))}
+        </ul>
+        
+        </div>
       </div>
     </div>
   </div>
-  
-)
-};
+  );
+}
 
-export default App;
-
+export default PokemonSearch;
